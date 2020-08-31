d945af4a7c03983d099703f115b91b363ba58c39
Arun Baby <arunbaby@Mac.local>
2020-08-31 15:33:14 +0530

    basic html

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "First post",
         "excerpt":"Hello world, and everyone.   Started this blog as a means to communicate to the world. I know I am gonna make a lot of mistakes here. I am not afraid to make mistakes anymore.   Task Lists      Blog daily   Update the website with relevent information  ","categories": ["general"],
         "tags": ["first","start"],
-        "url": "http://localhost:4000/arunbaby.com/general/first-post/",
+        "url": "http://localhost:4000/general/first-post/",
         "teaser": null
       }]
