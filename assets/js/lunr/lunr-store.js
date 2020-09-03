e084d4d0ba8b1457d0e630dfa61dc533fbfe4aa8
Arun Baby <arunbaby@Mac.local>
2020-09-03 23:18:21 +0530

    3rd post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -16,4 +16,10 @@ var store = [{
         "tags": ["education","learning"],
         "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/learning-vs-education/",
         "teaser": null
+      },{
+        "title": "Conformist",
+        "excerpt":"If everything you believe is something that you are supposed to believe, what is the odds that it is really a coincidence?   How many of your opinions are you reluctant to express inform of your friends/colleagues? Why so?   If the answer to the above question is ‘None’, step back and give it a thought. Odds are you are thinking in a way that you are told.   The other way to get the same answer is that you independently thought about each and every possibility for each of these scenarios and end up having the exact same answer. This is a highly unlikely scenario for obvious reasons.   Artists put voluntary mistakes so that they can identify when someone copies their work. So are mapmakers.   What are we trading for being conformist? If every generation had only conformists, the science/technology/medicine wouldn’t have evolved this much. It’s the outlaws/renegades/uncoventionals that bring real change to the world.   Use your brain, start thinking about why you are thinking what you think? Are you just already too much programmed?  ","categories": ["thoughts"],
+        "tags": ["conformist","learning"],
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/conformist/",
+        "teaser": null
       }]
