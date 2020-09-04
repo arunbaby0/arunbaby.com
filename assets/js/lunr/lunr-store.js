a7512ce1f33473338cf7f7d16ea02d97c8934a24
Arun Baby <arunbaby@Mac.local>
2020-09-04 23:04:04 +0530

    4 post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,24 +2,30 @@ var store = [{
         "title": "First post",
         "excerpt":"Hello world, and everyone.   Started this blog as a means to communicate to the world. I know I am gonna make a lot of mistakes here. I am not afraid to make mistakes anymore.   Task Lists      Blog daily   Update the website with relevent information  ","categories": ["general"],
         "tags": ["first","start"],
-        "url": "https://arunbaby0.github.io/arunbaby.com/general/first-post/",
+        "url": "http://localhost:4001/arunbaby.com/general/first-post/",
         "teaser": null
       },{
         "title": "Pleasure vs happiness",
         "excerpt":"The former is about taking, whereas the latter is all about giving. One is short-lived and the other is long-lived.   How many of us prioritise happiness over pleasure? When does the pleasure become indulgence? That last bag of Cheetos, one more episode in the NetFlix past midnight… Are those really helping us?   Please don’t confuse self-care with pleasure. Self-care is a necessity whereas the pleasure is completely optional.   The things which build up over a long term which doesn’t give instant joy mostly is related to happiness. For example, exercise, building skills can all bring happiness in the long run.   Is pleasure evil? No. It’s the imbalance which creates a real problem.   Which one do we long for? Which one should we prioritise? How do we balance? The real battle is within.  ","categories": ["thoughts"],
         "tags": ["pleasure","happiness"],
-        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/pleasure-vs-happiness/",
+        "url": "http://localhost:4001/arunbaby.com/thoughts/pleasure-vs-happiness/",
         "teaser": null
       },{
         "title": "Learning vs Education",
         "excerpt":"Education is the industrial process of making people compliant. Command and control is the backbone of it.  While learning is unleashing of a curious mind against the unknown. It is wandering through foreign territories. It can be guided or unguided.   The results that anyone wants to obtain through education is well defined. It can be a certificate or a degree. An external agency sets the limits for industrial education. The learner sets the limit for the learning.  Learning must be an engaging journey.   If you want to learn how to bicycle, don’t read a book, get on one and make mistakes. The fastest way to learn anything is by actively involving in an activity.   In the internet age, all you want to learn is a click away. Are we making use of all the resources that we have?      The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn, unlearn, and relearn. – Alvin Toffler    Please don’t confuse education with learning.   How much are you learning? What all you want to discover? What stops you from acquiring that skill that you always wanted to master?  ","categories": ["thoughts"],
         "tags": ["education","learning"],
-        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/learning-vs-education/",
+        "url": "http://localhost:4001/arunbaby.com/thoughts/learning-vs-education/",
         "teaser": null
       },{
         "title": "Conformist",
         "excerpt":"If everything you believe is something that you are supposed to believe, what is the odds that it is really a coincidence?   How many of your opinions are you reluctant to express inform of your friends/colleagues? Why so?   If the answer to the above question is ‘None’, step back and give it a thought. Odds are you are thinking in a way that you are told.   The other way to get the same answer is that you independently thought about each and every possibility for each of these scenarios and end up having the exact same answer. This is a highly unlikely scenario for obvious reasons.   Artists put voluntary mistakes so that they can identify when someone copies their work. So are mapmakers.   What are we trading for being conformist? If every generation had only conformists, the science/technology/medicine wouldn’t have evolved this much. It’s the outlaws/renegades/uncoventionals that bring real change to the world.   Use your brain, start thinking about why you are thinking what you think? Are you just already too much programmed?  ","categories": ["thoughts"],
         "tags": ["conformist","learning"],
-        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/conformist/",
+        "url": "http://localhost:4001/arunbaby.com/thoughts/conformist/",
+        "teaser": null
+      },{
+        "title": "Catch up with status quo",
+        "excerpt":"What if you could get rid of the idea that you always need to catch-up with your peers status wise? What changes will you make in your life? What stops you from doing that?   Will you hold on to your job/relationships/activities? Do you want that shiny new object? Is that object a necessity or just a show-off to others?      Advertising has us chasing cars and clothes, working jobs we hate so we can buy shit we don’t need. We’re the middle children of history, man. No purpose or place. We have no Great War. No Great Depression. Our Great War’s a spiritual war… our Great Depression is our lives. We’ve all been raised on television to believe that one day we’d all be millionaires, and movie gods, and rock stars. But we won’t. And we’re slowly learning that fact. And we’re very, very pissed off. — fight club    Are the things we think we want are the things that we genuinely want? Or, someone had programmed you into thinking that you want it.   What if you could relive yesterday? What changed would you make? What changes are you making for tomorrow?  ","categories": ["thoughts"],
+        "tags": ["status"],
+        "url": "http://localhost:4001/arunbaby.com/thoughts/status-game/",
         "teaser": null
       }]
