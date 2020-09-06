f8c009a2c22fa869be8e8ca536b9902c894c911c
Arun Baby <arunbaby@Mac.local>
2020-09-07 00:43:12 +0530

    6 post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -34,4 +34,10 @@ var store = [{
         "tags": ["experience"],
         "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/evaluated-experience/",
         "teaser": null
+      },{
+        "title": "Evaluated Experience",
+        "excerpt":"Is someone separate from their perspective? How can we disentangle these two?   We often have some hard feeling towards someone because of the perspective of that person, but we equate the person to the perspective. They might have expressed something, which irritated you, because of their particular perspective.   A person’s perspective is not permanent. It changes for many reasons. It would be very easy to understand people once we understand this concept. Trying to understand the person’s perspective often opens the door to strengthen the relationship.   How do you separate the person from the perspective? Whenever you try to judge someone, take a pause and think why that person is thinking that way? This is applicable for ourself as well. Think about why we are thinking what we think to open the door to a different level of understanding.   P.S. If you are interested google “metacognition” and go down the rabbit hole.  ","categories": ["thoughts"],
+        "tags": ["perspective"],
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/person-and-perspective/",
+        "teaser": null
       }]
