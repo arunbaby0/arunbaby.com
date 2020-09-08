1c1442321607fa0a88a8ab2ba2c1f4b79f6cfc02
Arun Baby <arunbaby@Mac.local>
2020-09-08 23:31:32 +0530

    8 post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -46,4 +46,10 @@ var store = [{
         "tags": ["leverage"],
         "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/leverage/",
         "teaser": null
+      },{
+        "title": "Non-resistance",
+        "excerpt":"The dictionary meaning “the principles or practice of passive submission to constituted authority even when unjust or oppressive” doesn’t capture the eastern philosophy of non-resistance.      Don’t get set into one form, adapt it and build your own, and let it grow, be like water. Empty your mind, be formless, shapeless — like water. Now you put water in a cup, it becomes the cup; You put water into a bottle it becomes the bottle; You put it in a teapot it becomes the teapot. Now water can flow or it can crash. Be water, my friend. - Bruce lee    The eastern philosophy is about resistance to growth, whether it is physical or mental. It is freedom from the resistance that’s built by ourself. In soft martial arts, we don’t suppress the blow from the opponent using any force, we just work around it and direct it back to the person.   When the focus is on the aspects of how things should have been, or the way someone treated us, when we rely on the perceived norms of society, when we cannot accept things as they are, we are being resistant.   It is similar to the idea of getting identified with labels. You are resisting the growth once you start identifying with labels.   How can we practise non-resistance? What barriers that we have already created in our minds? Be like water, my friend.  ","categories": ["thoughts"],
+        "tags": ["non-resistance"],
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/non-resistance/",
+        "teaser": null
       }]
