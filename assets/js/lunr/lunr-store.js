905f55a150cc12b07814c33fab65e2b54afc370f
Arun Baby <arunbaby@Mac.local>
2020-09-10 00:00:54 +0530

    9 post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -52,4 +52,10 @@ var store = [{
         "tags": ["non-resistance"],
         "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/non-resistance/",
         "teaser": null
+      },{
+        "title": "Entitlement",
+        "excerpt":"Are we entitled to have something for sure? Are we even entitled to think about our entitlement?   Is there a good amount of entitlement that we need to have? Can it be an exaggerated feeling of self-importance?   A study had shown that some amount of entitlement is linked to increased creativity.   When we think we are entitled to something, what is the basis on which we are making that assumption? How can we validate that assumption?   Can we avoid disappointment if we just avoid the feeling of entitlement? Are we even entitled for the necessities? The things that we take for granted, does it make to rethink about those? Especially with the pandemic where it forced us to rethink many things.   Is entitlement another label that we carry, which on removal give mukthi for us?  ","categories": ["thoughts"],
+        "tags": ["entitlement"],
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/entitlement/",
+        "teaser": null
       }]
