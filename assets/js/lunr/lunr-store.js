83cc6c8801a1f23af62fa667ac052738490aff8e
Arun Baby <arunbaby@Mac.local>
2020-09-11 23:48:24 +0530

    11 post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,66 +2,72 @@ var store = [{
         "title": "First post",
         "excerpt":"Hello world, and everyone.   Started this blog as a means to communicate to the world. I know I am gonna make a lot of mistakes here. I am not afraid to make mistakes anymore.   Task Lists      Blog daily   Update the website with relevent information  ","categories": ["general"],
         "tags": ["first","start"],
-        "url": "http://localhost:4001/arunbaby.com/general/first-post/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/general/first-post/",
         "teaser": null
       },{
         "title": "Pleasure vs happiness",
         "excerpt":"The former is about taking, whereas the latter is all about giving. One is short-lived and the other is long-lived.   How many of us prioritise happiness over pleasure? When does the pleasure become indulgence? That last bag of Cheetos, one more episode in the NetFlix past midnight… Are those really helping us?   Please don’t confuse self-care with pleasure. Self-care is a necessity whereas the pleasure is completely optional.   The things which build up over a long term which doesn’t give instant joy mostly is related to happiness. For example, exercise, building skills can all bring happiness in the long run.   Is pleasure evil? No. It’s the imbalance which creates a real problem.   Which one do we long for? Which one should we prioritise? How do we balance? The real battle is within.  ","categories": ["thoughts"],
         "tags": ["pleasure","happiness"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/pleasure-vs-happiness/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/pleasure-vs-happiness/",
         "teaser": null
       },{
         "title": "Learning vs Education",
         "excerpt":"Education is the industrial process of making people compliant. Command and control is the backbone of it.  While learning is unleashing of a curious mind against the unknown. It is wandering through foreign territories. It can be guided or unguided.   The results that anyone wants to obtain through education is well defined. It can be a certificate or a degree. An external agency sets the limits for industrial education. The learner sets the limit for the learning.  Learning must be an engaging journey.   If you want to learn how to bicycle, don’t read a book, get on one and make mistakes. The fastest way to learn anything is by actively involving in an activity.   In the internet age, all you want to learn is a click away. Are we making use of all the resources that we have?      The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn, unlearn, and relearn. – Alvin Toffler    Please don’t confuse education with learning.   How much are you learning? What all you want to discover? What stops you from acquiring that skill that you always wanted to master?  ","categories": ["thoughts"],
         "tags": ["education","learning"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/learning-vs-education/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/learning-vs-education/",
         "teaser": null
       },{
         "title": "Conformist",
         "excerpt":"If everything you believe is something that you are supposed to believe, what is the odds that it is really a coincidence?   How many of your opinions are you reluctant to express inform of your friends/colleagues? Why so?   If the answer to the above question is ‘None’, step back and give it a thought. Odds are you are thinking in a way that you are told.   The other way to get the same answer is that you independently thought about each and every possibility for each of these scenarios and end up having the exact same answer. This is a highly unlikely scenario for obvious reasons.   Artists put voluntary mistakes so that they can identify when someone copies their work. So are mapmakers.   What are we trading for being conformist? If every generation had only conformists, the science/technology/medicine wouldn’t have evolved this much. It’s the outlaws/renegades/uncoventionals that bring real change to the world.   Use your brain, start thinking about why you are thinking what you think? Are you just already too much programmed?  ","categories": ["thoughts"],
         "tags": ["conformist","learning"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/conformist/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/conformist/",
         "teaser": null
       },{
         "title": "Catch up with status quo",
         "excerpt":"What if you could get rid of the idea that you always need to catch-up with your peers status wise? What changes will you make in your life? What stops you from doing that?   Will you hold on to your job/relationships/activities? Do you want that shiny new object? Is that object a necessity or just a show-off to others?      Advertising has us chasing cars and clothes, working jobs we hate so we can buy shit we don’t need. We’re the middle children of history, man. No purpose or place. We have no Great War. No Great Depression. Our Great War’s a spiritual war… our Great Depression is our lives. We’ve all been raised on television to believe that one day we’d all be millionaires, and movie gods, and rock stars. But we won’t. And we’re slowly learning that fact. And we’re very, very pissed off. — fight club    Are the things we think we want are the things that we genuinely want? Or, someone had programmed you into thinking that you want it.   What if you could relive yesterday? What changed would you make? What changes are you making for tomorrow?  ","categories": ["thoughts"],
         "tags": ["status"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/status-game/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/status-game/",
         "teaser": null
       },{
         "title": "Evaluated Experience",
         "excerpt":"It is a standard consensus that experience is the best teacher. How does just the experience can be the best teacher?   A person can have the same experience multiple times and still doesn’t learn anything. Think about a person who gets into relationship after relationship and just fail every time. What could that person have done differently?   Evaluated experience is the best teacher. Yes, evaluation is painful, so is not changing.   An evaluation may be painful for most of us. The length of the suffering can be reduced significantly if we are ready to evaluate and learn from the experience.  Others may cause pain, but we are causing suffering for us. Unfortunately, letting go is not that simple.   What experiences are worth evaluating? How often are we evaluating? What is the basis for the evaluation? All these are decided by us. The power is with us.   ","categories": ["thoughts"],
         "tags": ["experience"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/evaluated-experience/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/evaluated-experience/",
         "teaser": null
       },{
         "title": "Person and perspective",
         "excerpt":"Is someone separate from their perspective? How can we disentangle these two?   We often have some hard feeling towards someone because of the perspective of that person, but we equate the person to the perspective. They might have expressed something, which irritated you, because of their particular perspective.   A person’s perspective is not permanent. It changes for many reasons. It would be very easy to understand people once we understand this concept. Trying to understand the person’s perspective often opens the door to strengthen the relationship.   How do you separate the person from the perspective? Whenever you try to judge someone, take a pause and think why that person is thinking that way? This is applicable for ourself as well. Think about why we are thinking what we think to open the door to a different level of understanding.   P.S. If you are interested google “metacognition” and go down the rabbit hole.  ","categories": ["thoughts"],
         "tags": ["perspective"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/person-and-perspective/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/person-and-perspective/",
         "teaser": null
       },{
         "title": "Leverage",
         "excerpt":"Leverage is something you can use to get maximum advantage of something. It is a tool. For you to use a tool, first you have to understand the fundamental principles about it.   In the age of the internet where every information ever created is accessible instantly with a few click, we have so much potential to create a greater good.   Just like any tool, things can be used in good and bad ways(even though the good and bad is just relative). You need to put in a lot of effort to master tools.      Give me a lever long enough and a fulcrum on which to place it, and I shall move the world.- Archimedes    What is the best tool to master? Mind? It is also the hardest to master. How much time and effort are you putting to master your mind? The first and greatest battle is always within.   How conscious are we in choosing our tools? How much mastery do you have over it? How are you planning to master new tools?  ","categories": ["thoughts"],
         "tags": ["leverage"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/leverage/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/leverage/",
         "teaser": null
       },{
         "title": "Non-resistance",
         "excerpt":"The dictionary meaning “the principles or practice of passive submission to constituted authority even when unjust or oppressive” doesn’t capture the eastern philosophy of non-resistance.      Don’t get set into one form, adapt it and build your own, and let it grow, be like water. Empty your mind, be formless, shapeless — like water. Now you put water in a cup, it becomes the cup; You put water into a bottle it becomes the bottle; You put it in a teapot it becomes the teapot. Now water can flow or it can crash. Be water, my friend. - Bruce lee    The eastern philosophy is about resistance to growth, whether it is physical or mental. It is freedom from the resistance that’s built by ourself. In soft martial arts, we don’t suppress the blow from the opponent using any force, we just work around it and direct it back to the person.   When the focus is on the aspects of how things should have been, or the way someone treated us, when we rely on the perceived norms of society, when we cannot accept things as they are, we are being resistant.   It is similar to the idea of getting identified with labels. You are resisting the growth once you start identifying with labels.   How can we practise non-resistance? What barriers that we have already created in our minds? Be like water, my friend.  ","categories": ["thoughts"],
         "tags": ["non-resistance"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/non-resistance/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/non-resistance/",
         "teaser": null
       },{
         "title": "Entitlement",
         "excerpt":"Are we entitled to have something for sure? Are we even entitled to think about our entitlement?   Is there a good amount of entitlement that we need to have? Can it be an exaggerated feeling of self-importance?   A study had shown that some amount of entitlement is linked to increased creativity.   When we think we are entitled to something, what is the basis on which we are making that assumption? How can we validate that assumption?   Can we avoid disappointment if we just avoid the feeling of entitlement? Are we even entitled for the necessities? The things that we take for granted, does it make to rethink about those? Especially with the pandemic where it forced us to rethink many things.   Is entitlement another label that we carry, which on removal give mukthi for us?  ","categories": ["thoughts"],
         "tags": ["entitlement"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/entitlement/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/entitlement/",
         "teaser": null
       },{
         "title": "Intelligence",
         "excerpt":"Are we intelligent? Will any intelligent person think themself of intelligent?   An intelligent person will be aware of the loopholes in their thinking. And someone who thinks of themselves as intelligent may be unaware of their state.      The measure of intelligence is the ability to change - Albert Einstein.    This quote goes per darwin’s theory of evolution. The entire theory points out the importance of adaptability.      Don’t confuse education with intelligence - unknown    The compliance training(aka education) has nothing to do with intelligence. Also, the real learning doesn’t have to be for any labels.   What if you stop trying to look intelligent and start trying to do something important? Isn’t that a better way to learn? An intelligent way?  ","categories": ["thoughts"],
         "tags": ["Intelligence"],
-        "url": "http://localhost:4001/arunbaby.com/thoughts/intelligence/",
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/intelligence/",
+        "teaser": null
+      },{
+        "title": "Winning a game",
+        "excerpt":"What does it mean to win a game? Many people try to win every game. But at what cost, what was the point of winning?   We are all playing one game or the other 24x7. Are we conscious of the games that we are playing?   We define the rules sometimes for the games; Some time its others. Sometimes we play the games we know we can’t win.   Sometimes we try to modify the rules of the game while we are playing, to increase the odds of our winning. what greatness is there in that kind of wins. You could have chosen the right game to play in the first place.   In some games, both parties can win. This increases the odds of having a good relationship with the other players. In some, we have to trash others to win the game. This often ends up in toxic relationships.   Many games are played in the mind initially. Why are we even obsessed with winning the game, if we don’t care about the outcomes?   The best strategy to win at some of the games is just by not playing it.  Our economy thrives on selling our attention to advertisers. When a battalion of scientists fights for our attention, by all means, the best strategy to win that game is not by playing it.  ","categories": ["thoughts"],
+        "tags": ["winning","game"],
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/winning-a-game/",
         "teaser": null
       }]
