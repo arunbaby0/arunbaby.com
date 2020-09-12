aa5fef519572bb1e0a515e69db1e52775e4248d9
Arun Baby <arunbaby@Mac.local>
2020-09-12 23:54:56 +0530

    12 post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -70,4 +70,10 @@ var store = [{
         "tags": ["winning","game"],
         "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/winning-a-game/",
         "teaser": null
+      },{
+        "title": "What is the most difficult 3 words to say?",
+        "excerpt":"“I don’t know”. It is the acceptance of our limited knowledge.   Does our culture encourage us to practise this? Not knowing is mostly associated with ignorance for most people.   Why is it difficult for us to see the limitations of our perspective? Is it difficult for a fish to see water?   There can have no argument between two people who says “I don’t know”, It will only be a discussion.   Does acknowledging our limited knowledge make us any inferior person?  Once you admit to not knowing, your knowledge will have much greater value.   Once we accept the fact that we don’t know, the real possibility of exploration starts. Anything is possible once we follow our curiosity.   Do we need to know everything in the entire cosmos? Not really, I think. We hardly need to know anything.   Do I know the answers to the above questions at least? well, I DON’T.  ","categories": ["thoughts"],
+        "tags": ["difficult","dont"],
+        "url": "https://arunbaby0.github.io/arunbaby.com/thoughts/most-difficult-three-words/",
+        "teaser": null
       }]
